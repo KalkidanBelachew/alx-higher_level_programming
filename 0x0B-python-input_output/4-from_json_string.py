0eeac59b1876e66b46c9b9c5c15785edf9c68209
KalkidanBelachew <kal.belachew.18@gmail.com>
2022-07-06 22:05:28 +0300

    Input/Output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Module 4.
-Appends a string at the end of a text file.
+"""Module 4-from_json_string.
+Returns an object (Python data structure)
+represented by a JSON string.
 """
+
+
 import json
 
 
 def from_json_string(my_str):
+    """Return the object represented my my_str.
+    Args:
+        - my_str: JSON string representation
+    Returns: corresponding object
     """
-    Convert a json string to a python object
-    Arguments:
-        my_obj (str): The inputed object to convert in json format
-    Return:
-        A Python object from json
-    """
+
     return json.loads(my_str)
 
 
