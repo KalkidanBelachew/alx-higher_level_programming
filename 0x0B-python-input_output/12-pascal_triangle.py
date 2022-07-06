e553225044e6a52d2d6f89aa1a5fe6b54f0d30b2
KalkidanBelachew <kal.belachew.18@gmail.com>
2022-07-06 22:44:23 +0300

    Input/Output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,37 +1,26 @@
 #!/usr/bin/python3
-"""Module 12-student.
-Creates a Student class.
+"""Module 12-pascal_triangle.
+Returns a list of lists of integers
+representing the Pascal’s triangle of n.
 """
 
-
-class Student:
-    """Class that defines a student.
-    Public attributes:
-        - first_name
-        - last_name
-        - age
-    Public method to_json().
+def pascal_triangle(n):
     """
+    Creates a pascal triangle
+    Attributes:
+        n (int): The n exponent for triangle
+    Return:
+        A matrix with values for the triangle
+    """
+    pascal = []
+    triangle = []
 
-    def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance."""
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Retrieves a dictionary representation
-        of a Student instance.
-        Args:
-            - attrs: list of attributes
-        Returns: the dict representation of the instance.
-        """
-
-        my_dict = dict()
-        if type(attrs) is list and all(type(x) is str for x in attrs):
-            for x in attrs:
-                if x in self.__dict__:
-                    my_dict.update({x: self.__dict__[x]})
-            return my_dict
-        return self.__dict__.copy()
+    for i in range(int(n)):
+        new = pascal[:]
+        new.append(1)
+        pos = len(pascal)
+        for i in range(1, pos):
+            new[i] = pascal[i - 1] + pascal[i]
+        pascal = new[:]
+        triangle.append(pascal)
+    return `
